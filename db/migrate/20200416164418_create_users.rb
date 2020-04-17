4a331292feaee0fcf9eb7a23a1aa067591472d4b
ilya <sad1lovbatya@gmail.com>
2020-04-17 07:43:50 -0400

    validation emails db

--- db/migrate/20200416164418_create_users.rb
+++ db/migrate/20200416164418_create_users.rb
@@ -1,5 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
+    add_index :users, :email, unique: true
+    
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
