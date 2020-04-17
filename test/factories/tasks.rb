47738672e8cbf985b64b25b732fa07ddcd68fb1a
ilya <sad1lovbatya@gmail.com>
2020-04-17 04:39:01 -0400

    add traits author and assigne

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,10 +2,8 @@ FactoryBot.define do
   factory :task do
     name   
     description   
-    author_id   
-    assignee_id  
-    author { create :user }
-    state  
+    author { nil }
+    assigne { nil }
     expired_at  
   end
 end
