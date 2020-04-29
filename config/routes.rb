3b7e1ff3713221981157921c45e11f896f42565a
ilya <sad1lovbatya@gmail.com>
2020-04-29 14:29:44 -0400

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   scope module: :web do
     resource :board, only: :show
-    resource :session, only: [:new, :create, :destroy]
-    resources :developers, only: [:new, :create]
+    resource :session, only: %i[new create destroy]
+    resources :developers, only: %i[new create]
   end
 end
