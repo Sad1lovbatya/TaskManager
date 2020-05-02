782f15403366f9619f44ea2a4b6c48a4a2f63368
ilya <sad1lovbatya@gmail.com>
2020-05-02 07:02:20 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost')
-admin.password = 'admin'
+admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost.com')
+admin.password = '12345678'
 admin.save
 
 60.times do |i|
   u = [Manager, Developer].sample.new
-  u.email = "email#{i}@mail.gen"
+  u.email = "email#{i}mail.gen@"
   u.first_name = "FN#{i}"
   u.last_name = "LN#{i}"
   u.password = "#{i}"
